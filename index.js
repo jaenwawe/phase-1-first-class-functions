20ff0e53118e739c65438df29e6352c9559ee5f5
jaenwawe <nwawe.jae@gmail.com>
2021-08-13 15:27:43 -0500

    commit tree 7dd4a074c1733efc44b483ab645788051bf6e109

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function greetings(name){
+    const classmate= console.log(`Hi ${name}!`)
+    return classmate
+    }
+    
+
+function receivesAFunction (greetings){
+    greetings('One')
+    }
+
+
+
+
+    function returnsANamedFunction() {
+        let sticks = "gum";
+        return gumSticks
+        }
+
+    function gumSticks () {
+        return console.log(`The gum sticks are delicious!`)
+     }
+     
+
+        function returnsAnAnonymousFunction(){  
+     
+            return function () {
+                console.log(`Its after 5`);
+              };
+            }
+   
+     
